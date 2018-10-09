bc61453f44836010451e0e6fe2972c9d2106f318
Christoph Kastius <kastius@gmail.com>
2018-10-09 09:47:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
 
-$host = getenv('HTTP_HOST');
+$host = $_SERVER['SERVER_NAME'];
 
 if($host == "www.domain.de"){
-    header("Location: http://www.domain.de/de.htm");
-}
-
-else if($host == "www.domain.com"){
-    header("Location: http://www.domain.com/folder/index.html");
-}
-
-else if($host == "www.domain.net"){
-    header("Location: http://www.domain.net/net.htm");
-}
-
-else if($host == "www.domain.org"){
-    header("Location: http://www.domain2.ch/org.html");
+    header("Location: http://www.domain.de/de.php");
 }
 
 else if($host == "www.domain.at"){
-    header("Location: http://www.domain.at/at.htm");
+    header("Location: http://www.domain.at/at.php");
+}
+
+else if($host == "www.domain.com"){
+    header("Location: http://www.domain.com/com.php");
+}
+
+else if($host == "www.domain.net"){
+    header("Location: http://www.domain.net/net.php");
+}
+
+else if($host == "www.domain.org"){
+    header("Location: http://www.domain.org/org.php");
 }
 
 else if($host == "www.domain.ch"){
-    header("Location: http://www.domain.ch/ch.htm");
+    header("Location: http://www.domain.ch/ch.php");
 }
 
 else if($host == "www.domain.li"){
-    header("Location: http://www.domain.li/li.htm");
+    header("Location: http://www.domain.li/li.php");
 }
 ?>
